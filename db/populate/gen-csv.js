7ff6a3e91ab310abcea3a48c184e4fe6784596ec
carinij <carinij@gmail.com>
2021-07-10 02:07:09 -0700

    Various improvements; now reliably creates 10M+ records for Books and BooksCategories that won't have any duplicate keys or other issues.

--- db/populate/gen-csv.js
+++ db/populate/gen-csv.js
@@ -1,15 +1,6 @@
-const csvWriter = require('csv-write-stream');
 const fs = require('fs');
 const faker = require('faker');
 const { Stream } = require('stream');
-
-const coinflip = () => {
-  if (Math.floor(Math.random() * 2) === 1) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 const getRandom = (max) => {
   return (Math.floor(Math.random() * max));
@@ -71,7 +62,7 @@ const createCategories = (number) => {
       }
     }
     const createdAtDate = generateCreatedAt();
-    resultObject[stringToPush] = stringToPush + "," + createdAtDate.toISOString() + "," + generateUpdatedAt(createdAtDate).toISOString()  + "\n";
+    resultObject[stringToPush] = stringToPush + ", " + createdAtDate.toISOString() + ", " + generateUpdatedAt(createdAtDate).toISOString()  + "\n";
   }
   for (let key in resultObject) {
     readable.push(resultObject[key]);
@@ -86,19 +77,19 @@ const createBooks = (number) => {
   let currentChunk = 1;
   const readable = new Stream.Readable();
   readable.pipe(fs.createWriteStream('books.csv'));
-  readable.push("title,subtitle,author,narrator,imageUrl,audioSampleUrl,length,version,createdAt,updatedAt\n");
+  readable.push("title, subtitle, author, narrator, imageUrl, audioSampleUrl, length, version, createdAt, updatedAt\n");
   for (let i = 0; i < number; i++) {
     const createdAtDate = generateCreatedAt();
     const newBook = {
-      title: capitalizeAllFirsts(faker.company.bs()) + ",",
-      subtitle: capitalizeAllFirsts(faker.company.catchPhrase()) + ",",
-      author: faker.name.findName() + ",",
-      narrator: faker.name.findName() + ",",
-      imageUrl: faker.image.imageUrl() + ",",
-      audioSampleUrl: faker.internet.url() + ",",
+      title: capitalizeAllFirsts(faker.company.bs()) + ", ",
+      subtitle: capitalizeAllFirsts(faker.company.catchPhrase()) + ", ",
+      author: faker.name.findName() + ", ",
+      narrator: faker.name.findName() + ", ",
+      imageUrl: faker.image.imageUrl() + ", ",
+      audioSampleUrl: faker.internet.url() + ", ",
       length: (getRandom(29) + 4) + " hours and " + getRandom(60) + " minutes" + ",",
-      version: getRandom(100) < 85 ? "Unabridged Audiobook" + "," : "Abridged Audiobook" + ",",
-      createdAt: createdAtDate.toISOString() + ",",
+      version: getRandom(100) < 85 ? "Unabridged Audiobook" + ", " : "Abridged Audiobook" + ", ",
+      createdAt: createdAtDate.toISOString() + ", ",
       updatedAt: generateUpdatedAt(createdAtDate).toISOString()
     }
     let stringToPush = "";
@@ -116,5 +107,40 @@ const createBooks = (number) => {
   console.log("Finished pushing data to books.csv");
 }
 
-createCategories(200);
-createBooks(10000000);
+const createBooksCategories = (numberBooks) => {
+  const numberCategories = 200;
+  console.log("Creating bookscategories.csv with " + numberBooks + " records.");
+  let onePercent = numberBooks / 100;
+  let currentChunk = 1;
+  const readable = new Stream.Readable();
+  readable.pipe(fs.createWriteStream('bookscategories.csv'));
+  readable.push("bookId, categoryId\n");
+  const creationLoop = async () => {
+    for (let i = 0; i < numberBooks; i++) {
+      const firstCategory = getRandom(numberCategories) + 1;
+      let secondCategory = 0;
+      if ((getRandom(2) === 0 && firstCategory > 1) || firstCategory === 200) {
+        secondCategory = getRandom(firstCategory - 1) + 1;
+      } else {
+        secondCategory = getRandom(numberCategories - firstCategory - 1) + firstCategory + 1;
+      }
+      readable.push((i + 1) + ", " + (firstCategory) + "\n");
+      readable.push((i + 1) + ", " + (secondCategory) + "\n");
+      if (i === currentChunk * onePercent) {
+        console.log(currentChunk + "% complete.");
+        currentChunk++;
+        // Slowing down the writing a bit to avoid heap error that I don't understand
+        await new Promise(resolve => {
+          setTimeout(resolve, 500)
+        });
+      }
+    }
+    readable.push(null)
+    console.log("Finished pushing data to bookscategories.csv");
+  }
+  creationLoop();
+}
+
+// createCategories(200);
+// createBooks(10000000);
+// createBooksCategories(10000000);
