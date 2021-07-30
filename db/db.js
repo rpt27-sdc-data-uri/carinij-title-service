b272d75b201128819ff3096810c52ea3b2ad7d58
carinij <carinij@gmail.com>
2021-07-30 11:31:06 -0700

    Refactored to match schema used in data generation (removed id from BooksCategories, changed name of join table to BooksCategories, etc.)

--- db/db.js
+++ db/db.js
@@ -53,54 +53,47 @@ const Category = sequelize.define('Category', {
   }
 });
 
-// const Book_Category = sequelize.define('Book_Category', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   book_id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: false,
-//     unique: true,
-//     references: {
-//       model: Book,
-//       key: 'id',
-//     },
-//     onDelete: 'cascade',
-//     onUpdate: 'cascade',
-//   },
-//   category_id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: false,
-//     unique: true,
-//     references: {
-//       model: Category,
-//       key: 'id',
-//     },
-//     onDelete: 'cascade',
-//     onUpdate: 'cascade',
-//   }
-// }, {
-//   timestamps: false,
-//   freezeTableName: true
-// });
+const BooksCategories = sequelize.define('BooksCategories', {
+  bookId: {
+    type: DataTypes.INTEGER,
+    primaryKey: false,
+    references: {
+      model: Book,
+      key: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    primaryKey: false,
+    references: {
+      model: Category,
+      key: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  }
+}, {
+  timestamps: false,
+  freezeTableName: true
+});
 
-// Book.belongsToMany(Category, {
-//   through: "Book_Category",
-//   // as: 'categories',
-//   // foreignKey: 'book_id'
-// });
+Book.belongsToMany(Category, {
+  through: "BooksCategories",
+  as: 'categories',
+  foreignKey: 'bookId'
+});
 
-// Category.belongsToMany(Book, {
-//   through: "Book_Category",
-//   // as: 'books',
-//   // foreignKey: 'category_id'
-// });
+Category.belongsToMany(Book, {
+  through: "BooksCategories",
+  as: 'books',
+  foreignKey: 'categoryId'
+});
 
 sequelize.sync();
 
 module.exports.sequelize = sequelize;
 module.exports.Book = Book;
 module.exports.Category = Category;
-// module.exports.BookCategory = Book_Category;
+module.exports.BooksCategories = BooksCategories;
