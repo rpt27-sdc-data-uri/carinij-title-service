92e236d43d1caf49761e96a46070a787d3e2071d
carinij <carinij@gmail.com>
2021-08-05 20:17:56 -0700

    k6 load test script for get route

---
+++ test/loadTestGet.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+
+export let options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s',
+      duration: '10s',
+      preAllocatedVUs: 500,
+      maxVUs: 5000,
+    },
+  },
+};
+
+const getRandom = (max) => {
+  return (Math.floor(Math.random() * max));
+}
+
+export default function () {
+  const id = getRandom(10000000);
+  http.get(`http://localhost:2002/api/book/${id}`)
+}
